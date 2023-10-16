8e4a9222476beb7f09abeb5f7ae3d586de777b6f
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-16 12:34:55 +0000

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+napis = 'Akademia108';
+
+function reverseString(napis) {
+  return napis.reverse();
+};
+
+console.log(reverseString('Testowy napis 200 znaków'));
+
+
+/* Napisz funkcję, która pobiera jako parametr stringa (czyli napis) i zwraca ten sam napis tylko napisany wspak.
+
+Następnie wywołaj tą funkcję i odwróć wspak stringa "Akademia108", a wynik działania funkcji wyświetl w konsoli.
+
+Zadanie domowe robimy z wykorzystaniem serwisu https://repl.it
+
+Podpowiedź przed wysłaniem zadania do sprawdzenia:
+
+najprościej zadanie zrobić używając funkcji javascript do operacji na tablicach */
